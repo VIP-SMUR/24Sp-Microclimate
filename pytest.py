05ccc8d42aea104e3a159b14db8d7d2d34c69fd0
zeyujiang8800 <114033081+zeyujiang8800@users.noreply.github.com>
2024-02-24 14:46:09 -0500

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -3,14 +3,12 @@ from uwg import UWG
 # Define the .epw, .uwg paths to create an uwg object.
 epw_path = "uwg/resources/USA_GA_Atlanta.722190_TMY2.epw" # available in resources directory.
 
+# path for Atlanta Airport EPW: uwg/resources/USA_GA_Atlanta.722190_TMY2.epw
+# path for GT Campus EPW: uwg/resources/GT_33.77463936796479_-84.39704008595767_2020.epw
 
 # Initialize the UWG model by passing parameters as arguments, or relying on defaults
 model = UWG.from_param_args(epw_path=epw_path, bldheight=10, blddensity=0.5,
                             vertohor=0.8, grasscover=0.1, treecover=0.1, zone='1A')
-
-# Uncomment these lines to initialize the UWG model using a .uwg parameter file
-# param_path = "initialize_singapore.uwg"  # available in resources directory.
-# model = UWG.from_param_file(param_path, epw_path=epw_path)
 
 model.generate()
 model.simulate()
