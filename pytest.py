785c9567dcadc6edd396fc9e7fe0d65898275802
zeyujiang8800 <114033081+zeyujiang8800@users.noreply.github.com>
2024-02-24 14:54:54 -0500

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,10 +1,10 @@
 from uwg import UWG
 
 # Define the .epw, .uwg paths to create an uwg object.
-epw_path = "uwg/resources/USA_GA_Atlanta.722190_TMY2.epw" # available in resources directory.
+epw_path = "Sp24-Microclimate/resources/USA_GA_Atlanta.722190_TMY2.epw" # available in resources directory.
 
-# path for Atlanta Airport EPW: uwg/resources/USA_GA_Atlanta.722190_TMY2.epw
-# path for GT Campus EPW: uwg/resources/GT_33.77463936796479_-84.39704008595767_2020.epw
+# path for Atlanta Airport EPW: Sp24-Microclimate/resources/USA_GA_Atlanta.722190_TMY2.epw
+# path for GT Campus EPW: Sp24-Microclimate/resources/GT_33.77463936796479_-84.39704008595767_2020.epw
 
 # Initialize the UWG model by passing parameters as arguments, or relying on defaults
 model = UWG.from_param_args(epw_path=epw_path, bldheight=10, blddensity=0.5,
